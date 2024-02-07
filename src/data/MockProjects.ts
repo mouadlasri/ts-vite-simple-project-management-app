8c2ba0d3cf1b98bd5fcdbca84a05181da99532f9
Mouad Lasri <mouadkor20@gmail.com>
2024-02-07 12:19:03 +0900

    Added images in asset folder and updated image url inside MockProjects.ts

--- src/data/MockProjects.ts
+++ src/data/MockProjects.ts
@@ -1,11 +1,14 @@
 import { Project } from "../models/Project";
 
+/**
+ * Mock projects data.
+ */
 export const MOCK_PROJECTS = [
   new Project({
     id: 1,
     name: "Johnson - Kutch",
     description: "Fully-configurable intermediate framework. Ullam occaecati libero laudantium nihil voluptas omnis.",
-    imageUrl: "/assets/placeimg_500_300_arch4.jpg",
+    imageUrl: "/src/assets/placeimg_500_300_arch5.jpg", // Update the image URL to include the correct path
     contractTypeId: 3,
     contractSignedOn: "2013-08-04T22:39:41.473Z",
     budget: 54637,
@@ -16,7 +19,7 @@ export const MOCK_PROJECTS = [
     name: "Wisozk Group",
     description:
       "Centralized interactive application. Exercitationem nulla ut ipsam vero quasi enim quos doloribus voluptatibus.",
-    imageUrl: "/assets/placeimg_500_300_arch1.jpg",
+    imageUrl: "/src/assets/placeimg_500_300_arch1.jpg",
     contractTypeId: 4,
     contractSignedOn: "2012-08-06T21:21:31.419Z",
     budget: 91638,
@@ -27,7 +30,7 @@ export const MOCK_PROJECTS = [
     name: "Denesik LLC",
     description:
       "Re-contextualized dynamic moratorium. Aut nulla soluta numquam qui dolor architecto et facere dolores.",
-    imageUrl: "/assets/placeimg_500_300_arch12.jpg",
+    imageUrl: "/src/assets/placeimg_500_300_arch12.jpg",
     contractTypeId: 6,
     contractSignedOn: "2016-06-26T18:24:01.706Z",
     budget: 29729,
@@ -37,7 +40,7 @@ export const MOCK_PROJECTS = [
     id: 4,
     name: "Purdy, Keeling and Smitham",
     description: "Innovative 6th generation model. Perferendis libero qui iusto et ullam cum sint molestias vel.",
-    imageUrl: "/assets/placeimg_500_300_arch5.jpg",
+    imageUrl: "/src/assets/placeimg_500_300_arch5.jpg",
     contractTypeId: 4,
     contractSignedOn: "2013-05-26T01:10:42.344Z",
     budget: 45660,
@@ -47,7 +50,7 @@ export const MOCK_PROJECTS = [
     id: 5,
     name: "Kreiger - Waelchi",
     description: "Managed logistical migration. Qui quod praesentium accusamus eos hic non error modi et.",
-    imageUrl: "/assets/placeimg_500_300_arch12.jpg",
+    imageUrl: "/src/assets/placeimg_500_300_arch12.jpg",
     contractTypeId: 2,
     contractSignedOn: "2009-12-18T21:46:47.944Z",
     budget: 81188,
@@ -57,7 +60,7 @@ export const MOCK_PROJECTS = [
     id: 6,
     name: "Lesch - Waelchi",
     description: "Profound mobile project. Rem consequatur laborum explicabo sint odit et illo voluptas expedita.",
-    imageUrl: "/assets/placeimg_500_300_arch1.jpg",
+    imageUrl: "/src/assets/placeimg_500_300_arch1.jpg",
     contractTypeId: 3,
     contractSignedOn: "2016-09-23T21:27:25.035Z",
     budget: 53407,
