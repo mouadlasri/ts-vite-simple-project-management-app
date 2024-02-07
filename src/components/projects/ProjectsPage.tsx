600bdd3885e74b28e8850533cc6a3c72de24bee9
Mouad Lasri <mouadkor20@gmail.com>
2024-02-07 10:46:59 +0900

    Folder refactoring to follow best practices (separated "data" and "models")

--- src/components/projects/ProjectsPage.tsx
+++ src/components/projects/ProjectsPage.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 import "./ProjectsPage.css";
 
+import { MOCK_PROJECTS } from "../../data/MockProjects";
+
 export const ProjectsPage = () => {
   return (
     <div>
       <h1>Projects</h1>
+      <pre>{JSON.stringify(MOCK_PROJECTS, null, " ")}</pre>
     </div>
   );
 };
